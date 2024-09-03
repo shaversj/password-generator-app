8642f771b8e9cbf71f87de318ef1c951e60d908c
Josh Shavers <shaversj@gmail.com>
2024-09-03 10:08:15 -0500

    Add state and handlers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import CheckedSVG from "@/components/CheckedSVG";
+import IconArrowRightSVG from "@/components/IconArrowRightSVG";
+import IconCopySVG from "@/components/IconCopySVG";
+import { useState } from "react";
 
 export default function Home() {
+  const [passwordLength, setPasswordLength] = useState(10);
+
   const passwordCriteria = [
     {
       key: "uppercase",
@@ -20,17 +27,32 @@ export default function Home() {
     },
   ];
 
-  return (
-    <main className="">
-      <h1 className={"text-almost-white text-[32px] leading-[43px]"}>Password Generator</h1>
-      <div>{/* Display Password*/}</div>
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    // read form data
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const length = formData.get("length");
+    const criteria = passwordCriteria.filter((criteria) => formData.get(criteria.key));
+    console.log({ length, criteria });
+  }
 
-      <form>
+  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    setPasswordLength(parseInt(e.currentTarget.value));
+  }
+
+  return (
+    <main className="w-[540px]">
+      <h1 className={"text-grey text-center text-[24px] leading-[43px]"}>Password Generator</h1>
+      <div className={"bg-dark-grey text-almost-white mt-[31px] flex h-[80px] items-center px-8 py-[19px] text-[32px]"}>
+        PTx1f5DaFX <IconCopySVG className={"ml-auto"} />{" "}
+      </div>
+
+      <form className={"bg-dark-grey mt-6 p-8"} onSubmit={(e) => handleSubmit(e)}>
         <div className={"flex flex-col"}>
           <label className={"text-almost-white flex items-center text-[18px] leading-[23px]"} htmlFor={"length"}>
-            Character Length <span className={"text-neon-green ml-auto text-[32px] leading-[43px]"}>10</span>
+            Character Length <span className={"text-neon-green ml-auto text-[32px] leading-[43px]"}>{passwordLength}</span>
           </label>
-          <input className={"mt-4"} type={"range"} name={"length"} min={0} max={20} />
+          <input onChange={(e) => onChangeHandler(e)} className={"mt-4"} type={"range"} name={"length"} min={0} max={20} step={1} />
         </div>
 
         <div className={"space-y-5 pt-8"}>
@@ -38,12 +60,19 @@ export default function Home() {
             <div key={criteria.key} className={"flex items-center"}>
               <input className={"border-almost-white checked:bg-neon-green peer h-[1.25rem] w-[1.25rem] appearance-none border-2"} type={"checkbox"} name={criteria.key} />
               <CheckedSVG className={"pointer-events-none absolute hidden h-[1.25rem] w-[1.25rem] peer-checked:block"} />
-              <label className={"text-almost-white pl-6 text-[18px] leading-[23px]"} htmlFor={criteria.key}>
+              <label className={"text-almost-white pl-6 text-[18px] font-semibold leading-[23px]"} htmlFor={criteria.key}>
                 {criteria.displayName}
               </label>
             </div>
           ))}
         </div>
+
+        <button
+          type={"submit"}
+          className={"text-dark-grey bg-neon-green mt-8 flex h-[65px] w-full items-center justify-center gap-x-6 text-[18px] font-semibold leading-[23px]"}
+        >
+          GENERATE <IconArrowRightSVG className={""} />
+        </button>
       </form>
     </main>
   );
