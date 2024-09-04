82c659b1a5f82e0136c8346a1d5b6afe6f2c1af8
Josh Shavers <shaversj@gmail.com>
2024-09-04 08:11:46 -0500

    Add password generator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,58 +8,75 @@ import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
 export default function Home() {
-  const [passwordLength, setPasswordLength] = useState(0);
+  const [passwordLength, setPasswordLength] = useState([0, 10]);
+  const [password, setPassword] = useState("");
 
   const passwordCriteria = [
     {
-      key: "uppercase",
+      name: "uppercase",
       displayName: "Include Uppercase Letters",
+      pattern: "A",
     },
     {
-      key: "lowercase",
+      name: "lowercase",
       displayName: "Include Lowercase Letters",
+      pattern: "a",
     },
     {
-      key: "numbers",
+      name: "numbers",
       displayName: "Include Numbers",
+      pattern: "0",
     },
     {
-      key: "symbols",
+      name: "symbols",
       displayName: "Include Symbols",
+      pattern: "!",
     },
   ];
 
-  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-    // read form data
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const length = formData.get("length");
-    const criteria = passwordCriteria.filter((criteria) => formData.get(criteria.key));
-    console.log({ length, criteria });
+  const passwordOptions = {
+    uppercase: "A",
+    lowercase: "a",
+    numbers: "0",
+    symbols: "!",
+  };
+
+  function generatePassword(criteria: string, length: number) {
+    let randomize = require("randomatic");
+    return randomize(criteria, length);
   }
 
-  function onChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
-    setPasswordLength(parseInt(e.currentTarget.value));
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const selectedCriteria = passwordCriteria.filter((criteria) => formData.get(criteria.name));
+    let criteria = selectedCriteria.map((data) => passwordOptions[data.name as keyof typeof passwordOptions]).join("");
+    if (criteria === "") {
+      alert("Please select at least one criteria");
+      return;
+    }
+
+    setPassword(generatePassword(criteria, passwordLength[1]));
   }
 
   return (
     <main className="w-[540px]">
       <h1 className={"text-grey text-center text-[24px] leading-[43px]"}>Password Generator</h1>
       <div className={"bg-dark-grey text-almost-white mt-[31px] flex h-[80px] items-center px-8 py-[19px] text-[32px]"}>
-        PTx1f5DaFX <IconCopySVG className={"ml-auto"} />{" "}
+        {password} <IconCopySVG className={"ml-auto"} />{" "}
       </div>
 
       <form className={"bg-dark-grey mt-6 p-8"} onSubmit={(e) => handleSubmit(e)}>
         <div className={"flex flex-col"}>
-          <label className={"text-almost-white flex items-center text-[18px] leading-[23px]"} htmlFor={"length"}>
-            Character Length <span className={"text-neon-green ml-auto text-[32px] leading-[43px]"}>{passwordLength}</span>
+          <label className={"text-almost-white flex items-center text-[18px] leading-[23px]"} htmlFor={"range-slider"}>
+            Character Length <span className={"text-neon-green ml-auto text-[32px] leading-[43px]"}>{passwordLength[1]}</span>
           </label>
           {/*<input onChange={(e) => onChangeHandler(e)} className={"mt-4"} type={"range"} name={"length"} min={0} max={20} step={1} />*/}
           <RangeSlider
             id={"range-slider"}
             value={passwordLength}
             thumbsDisabled={[false, false]}
-            defaultValue={[0, 0]}
+            defaultValue={10}
             min={0}
             max={20}
             step={1}
@@ -71,10 +88,15 @@ export default function Home() {
 
         <div className={"space-y-5 pt-8"}>
           {passwordCriteria.map((criteria) => (
-            <div key={criteria.key} className={"flex items-center"}>
-              <input className={"border-almost-white checked:bg-neon-green peer h-[1.25rem] w-[1.25rem] appearance-none border-2"} type={"checkbox"} name={criteria.key} />
+            <div key={criteria.name} className={"flex items-center"}>
+              <input
+                className={"border-almost-white checked:bg-neon-green peer h-[1.25rem] w-[1.25rem] appearance-none border-2"}
+                type={"checkbox"}
+                name={criteria.name}
+                value={criteria.name}
+              />
               <CheckedSVG className={"pointer-events-none absolute hidden h-[1.25rem] w-[1.25rem] peer-checked:block"} />
-              <label className={"text-almost-white pl-6 text-[18px] font-semibold leading-[23px]"} htmlFor={criteria.key}>
+              <label className={"text-almost-white pl-6 text-[18px] font-semibold leading-[23px]"} htmlFor={criteria.name}>
                 {criteria.displayName}
               </label>
             </div>
