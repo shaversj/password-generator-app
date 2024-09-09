23a6180dd9155e8ccf12770a174a51ced8f65747
Josh Shavers <shaversj@gmail.com>
2024-09-09 10:31:57 -0500

    Update copy toast

--- src/components/PasswordDisplay.tsx
+++ src/components/PasswordDisplay.tsx
@@ -12,7 +12,7 @@ export default function PasswordDisplay({ password, onClickHandler, displayCopyT
     <div className={"bg-dark-grey text-almost-white mt-5 flex h-[5rem] items-center px-4 text-2xl leading-[31px] md:px-8 md:text-[2rem] md:leading-[43px]"}>
       {password ? password : <span className={"text-[#55545c]"}>P4$5W0rD!</span>}
       <Button type={"button"} variant={"secondary"} onClick={() => onClickHandler({ textToCopy: password })}>
-        {displayCopyToast ? <span className={""}>COPIED</span> : ""} <IconCopySVG className={"shrink"} />
+        {displayCopyToast ? <span className={"hidden md:block"}>COPIED</span> : ""} <IconCopySVG className={"shrink"} />
       </Button>
     </div>
   );
